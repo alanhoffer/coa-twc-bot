615fefc79a67748ba9663815014c6598f4e01ca9
Alan Hoffer <alanhoffer97@hotmail.com>
2025-06-06 14:33:05 -0300

    asd

--- commands/addsocial.js
+++ commands/addsocial.js
@@ -3,26 +3,6 @@ export default async function addSocialCommand({ client, channel, tags, message,
     const command = args.shift().toLowerCase();
 
     if (command === '!addsocial') {
-        
-        const username = tags.username;
 
-        const nickname = await userService.getNicknameBySocialId('twitch', username);
-        if (!nickname) {
-            client.say(channel, `@${username} Please register at !web.`);
-            return;
-        }
-        if (args.length < 2) {
-            client.say(channel, `@${username}, correct usage: !addsocial <discord;youtube> <socialId>`);
-            return;
-        }
-
-        const [socialType, socialId] = args;
-
-        try {
-            await userService.addSocialToNickname(nickname, socialType, socialId);
-            client.say(channel, `@${username}, social linked.`);
-        } catch (error) {
-            client.say(channel, `@${username}, error: ${error.message}`);
-        }
     }
 }
